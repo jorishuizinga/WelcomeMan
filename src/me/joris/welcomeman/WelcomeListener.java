c02ac1403b14f871a5ad81fb61cc65440e9c5daa
Joris <jorishuizinga@me.com>
2015-06-21 18:24:10 +0200

    Added testing message
    Added getting of current time and displayname for a more personal and
    informational message

--- src/me/joris/welcomeman/WelcomeListener.java
+++ src/me/joris/welcomeman/WelcomeListener.java
@@ -1,11 +1,26 @@
 package me.joris.welcomeman;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 public class WelcomeListener implements Listener{
+	
+	DateFormat time = new SimpleDateFormat("hh:mm");
+	Calendar calendar = Calendar.getInstance();
+	
+	String currentTime = time.format(calendar.getTime());
+	String monthName;
 	
 	@EventHandler
 	public void onLogin(PlayerJoinEvent e){
-		
+		Player player = e.getPlayer();
+		player.sendMessage("--------------------------------------------------------------");
+		player.sendMessage(ChatColor.GRAY + "Hello" + " " + ChatColor.GOLD + player.getDisplayName() + ChatColor.GRAY + "," + "the time is" + " " + currentTime);
+		player.sendMessage("Enjoy your stay!");
+		player.sendMessage("--------------------------------------------------------------");
 	}
 }
